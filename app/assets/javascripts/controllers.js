5713eddf286790e0a61d12c99993e2d15cc3605e
k-miyake <3katsu3hiro@gmail.com>
2015-02-20 07:09:57 +0900

    controllerを別ファイルに切り出した

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,3 +1,4 @@
-function HelloController($scope) {
-  $scope.greeting = {text: "Hello" };
-}
+var myApp = angular.module('myApp',[]);
+myApp.controller('DoubleController', ['$scope', function($scope) {
+  $scope.double = function(value) { return value * 2; };
+}]);
