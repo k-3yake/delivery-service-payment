89fe074e5e824372679f636f75879fb6568f3f2e
k-miyake <3katsu3hiro@gmail.com>
2015-02-22 14:57:13 +0900

    webdriver-managerの起動停止とprotractorの実行を行うタスクを作成

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,5 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        // dist配下のファイルをhttp://localhost:9000/で公開する
-        connect: {
-            options: {
-                port: 9000,
-                hostname: 'localhost'
-            },
-            my_target: {
-                options: {
-                    base: 'dist'
-                }
-            }
-        },
         // test/conf.jsの設定に従ってテストを実行
         protractor: {
             options: {
@@ -26,23 +14,35 @@ module.exports = function (grunt) {
         },
         shell: {
             options: {
-                stdout: true
+                stdout: false
             },
-
             // Selenium Serverをバックグラウンドで実行
-            selenium: {
+            webdriver_start: {
                 command: "webdriver-manager start",
                 options: {
-                    stdout: false,
                     async: true
                 }
             },
-
+            sleep: {
+                command: "sleep 5",
+                options: {
+                    async: false
+                }
+            },
             // Selenium Serverのインストールおよび更新
             protractor_install: {
                 command: "webdriver-manager update"
+            },
+            webdriver_stop: {
+                command: "ps -ef | grep selenium-server-standalone | grep -v grep | awk '{print $2}' | xargs kill -9",
+                options: {
+                    async: false
+                }
             }
         }
     });
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.registerTask('selenium', "protractor run" , function(){
+        grunt.task.run('shell:webdriver_start','shell:sleep','protractor:my_target','shell:webdriver_stop');
+    });
 };
