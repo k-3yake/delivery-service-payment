4f6410509fe6e372d9b9bb49fff764bd2d47e524
k-miyake <3katsu3hiro@gmail.com>
2015-02-24 19:33:22 +0900

    テストレポート用にdescribeの名前を変更

--- test/integration/todo-spec.js
+++ test/integration/todo-spec.js
@@ -1,4 +1,4 @@
-describe('アカウントの認証', function() {
+describe('integration.アカウントの認証', function() {
 	it('無料期間中はサービス利用可能', function() {
 		//Given:アカウントを新規登録
 		browser.get('http://localhost:9000/account/');
