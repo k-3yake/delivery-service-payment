d71a933969ffa533862605fc253f296564c1ac94
k-miyake <3katsu3hiro@gmail.com>
2015-02-24 18:38:39 +0900

    protractorのJunit形式のレポートを出力するように修正

--- test/integration/conf.js
+++ test/integration/conf.js
@@ -1,4 +1,14 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['todo-spec.js']
+    specs: ['todo-spec.js'],
+
+    framework: "jasmine2",
+    onPrepare: function() {
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            filePrefix: 'xmloutput',
+            savePath: 'target/test-reports'
+        }));
+    }
 };
