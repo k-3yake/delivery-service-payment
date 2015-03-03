76b043565fc1527d3e8d2d740bfe7bb713d48d18
k-miyake <3katsu3hiro@gmail.com>
2015-03-03 21:33:54 +0900

    2回目以降じゃないと成功しないので修正

--- test/integration/todo-spec.js
+++ test/integration/todo-spec.js
@@ -2,11 +2,11 @@ describe('integration.アカウントの認証', function() {
 	it('無料期間中はサービス利用可能', function() {
 		//Given:アカウントを新規登録
 		browser.get('http://localhost:9000/account/');
-		element(by.model('loginId')).sendKeys('aaaa');
+		element(by.model('loginId')).sendKeys(new Date().getTime());
 		element(by.buttonText('Save')).click();
 
 		var todoList = element.all(by.repeater('todo in todos'));
 		var status = element(by.binding('status'))
-		expect(status.getText()).toEqual('409')
+		expect(status.getText()).toEqual('200')
 	    });
     });
